3baa27d3d1269675e8eeadd6c0a41481a223679f
amitznati <amitznati@gmail.com>
2017-01-09 20:46:48 +0200

    Copy Views from blog

---
+++ resources/views/partials/_scripts.blade.php
@@ -0,0 +1,5 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    {!! Html::script('js/jquery-3.1.1.min.js') !!}
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    {!! Html::script('js/bootstrap.min.js') !!}
+    
