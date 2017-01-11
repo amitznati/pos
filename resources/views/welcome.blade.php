62aa402dbf8e6a8901f81bd0c35056425edcfb43
amitznati <amitznati@gmail.com>
2017-01-11 20:35:07 +0200

    load resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Font -->
+       <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         
         <!-- Styles -->
         <link rel="stylesheet" href="css/rowgrid.css">
@@ -53,49 +53,10 @@
 
 
     <script src="js/jquery-3.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.0/jquery-ui.js"></script>
+    <script src="js/jquery-ui.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.5.0/lodash.min.js"></script>
+    <script src="js/lodash.min.js"></script>
     <script src="gridstack/gridstack.js"></script>
     <script src="gridstack/gridstack.jQueryUI.js"></script>
-    <script type="text/javascript">
-        $(function () {
-            var options = {
-                float: true,
-                resizable: false
-            };
-            $('.grid-stack').gridstack(options);
-
-            new function () {
-                this.items = [
-                    {x: 0, y: 0, width: 2, height: 2},
-                    {x: 3, y: 1, width: 1, height: 2},
-                    {x: 4, y: 1, width: 1, height: 1},
-                    {x: 2, y: 3, width: 3, height: 1},
-//                    {x: 1, y: 4, width: 1, height: 1},
-//                    {x: 1, y: 3, width: 1, height: 1},
-//                    {x: 2, y: 4, width: 1, height: 1},
-                    {x: 2, y: 5, width: 1, height: 1}
-                ];
-
-                this.grid = $('.grid-stack').data('gridstack');
-
-                this.addNewWidget = function () {
-                    var node = this.items.pop() || {
-                                x: 12 * Math.random(),
-                                y: 5 * Math.random(),
-                                width: 1 + 3 * Math.random(),
-                                height: 1 + 3 * Math.random()
-                            };
-                    this.grid.addWidget($('<div><div class="grid-stack-item-content" /><div/>'),
-                        node.x, node.y, node.width, node.height);
-                    return false;
-                }.bind(this);
-
-                $('#add-new-widget').click(this.addNewWidget);
-                for(var i=0;i<5;i++)this.addNewWidget();
-            };
-
-        });
-    </script>
-</html>
+    <script type="text/javascript" src="js/mainpos.js"></script>
+</html> 
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	$items = ['name' => 'name value'];
+    return view('welcome')->withItemse($items);
 });
